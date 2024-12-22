ef02b1618f6b17532e295af86f1ae8e39b8393bd
Astrid Lauenstein <me@acqrel.com>
2024-12-22 09:31:13 +0100

    Parallelize

--- day22/part2.rs
+++ day22/part2.rs
@@ -1,44 +1,54 @@
 use std::io::BufRead;
-use std::iter::zip;
+use std::sync::atomic::AtomicU16;
+use std::sync::atomic::Ordering;
+
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 fn main() {
     let mut stdin = std::io::stdin().lock();
     let mut line = String::new();
 
-    let mut total_for = vec![0_u16; 19_usize.pow(4)];
-    let mut seen = vec![0_u32; (19_usize.pow(4) + 31) / 32];
+    let total_for: Vec<_> = (0..19_usize.pow(4)).map(|_| AtomicU16::new(0)).collect();
 
-    let hash = |diffs: [i8; 4]| {
-        (diffs[0] as usize + 9) * 19_usize.pow(3)
-            + (diffs[1] as usize + 9) * 19_usize.pow(2)
-            + (diffs[2] as usize + 9) * 19_usize.pow(1)
-            + (diffs[3] as usize + 9) * 19_usize.pow(0)
-    };
-
+    let mut inputs = Vec::new();
     while stdin.read_line(&mut line).unwrap() != 0 {
-        let mut n: u32 = line.trim().parse().unwrap();
-
-        let mut hist = [0; 4];
-        for n in seen.iter_mut() {
-            *n = 0;
-        }
-
-        for i in 0..2000 {
-            let last = n % 10;
-            n ^= 16777215 & (n << 6);
-            n ^= 16777215 & (n >> 5);
-            n ^= 16777215 & (n << 11);
-            let new = n % 10;
-            hist.rotate_left(1);
-            hist[3] = new as i8 - last as i8;
-            let hash = hash(hist);
-            if i >= 3 && seen[hash >> 5] & (1 << (hash & 31)) == 0 {
-                seen[hash >> 5] |= 1 << (hash & 31);
-                total_for[hash] += new as u16;
-            }
-        }
-
+        let n: u32 = line.trim().parse().unwrap();
+        inputs.push(n);
         line.clear();
     }
-    println!("{}", total_for.iter().max().unwrap());
+
+    inputs.par_iter().for_each_init(
+        || vec![0_u32; (19_usize.pow(4) + 31) / 32],
+        |seen, &(mut n)| {
+            for n in seen.iter_mut() {
+                *n = 0;
+            }
+
+            let mut hash = 0;
+            for i in 0..2000 {
+                let last = n % 10;
+                n ^= 16777215 & (n << 6);
+                n ^= 16777215 & (n >> 5);
+                n ^= 16777215 & (n << 11);
+
+                let new = n % 10;
+                let diff = (new + 9 - last) as usize;
+                hash = (hash * 19 + diff) % 19_usize.pow(4);
+
+                if i >= 3 && seen[hash >> 5] & (1 << (hash & 31)) == 0 {
+                    seen[hash >> 5] |= 1 << (hash & 31);
+                    total_for[hash].fetch_add(new as u16, Ordering::Relaxed);
+                }
+            }
+        },
+    );
+
+    println!(
+        "{}",
+        total_for
+            .iter()
+            .map(|n| n.load(Ordering::Relaxed))
+            .max()
+            .unwrap()
+    );
 }
