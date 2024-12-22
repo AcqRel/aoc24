daab6579ce188e3ab352f949e3b80a8455901360
Astrid Lauenstein <me@acqrel.com>
2024-12-22 08:58:59 +0100

    Small improvement

--- day22/part2.rs
+++ day22/part2.rs
@@ -7,28 +7,23 @@ fn main() {
 
     let mut total_for = HashMap::new();
     let mut score_for = HashMap::new();
-    let mut values = Vec::new();
-    let mut diffs = Vec::new();
 
     while stdin.read_line(&mut line).unwrap() != 0 {
         let mut n: u32 = line.trim().parse().unwrap();
 
-        values.clear();
-        diffs.clear();
-        for _ in 0..2000 {
+        score_for.clear();
+        let mut hist = [0; 4];
+        for i in 0..2000 {
             let last = n % 10;
             n ^= 16777215 & (n << 6);
             n ^= 16777215 & (n >> 5);
             n ^= 16777215 & (n << 11);
             let new = n % 10;
-            values.push(new as u8);
-            diffs.push(new as i8 - last as i8);
-        }
-
-        score_for.clear();
-        for (chunk, &digit) in diffs.windows(4).zip(&values[3..]) {
-            let chunk: [i8; 4] = chunk.try_into().unwrap();
-            score_for.entry(chunk).or_insert(digit);
+            hist.rotate_left(1);
+            hist[3] = new as i8 - last as i8;
+            if i >= 3 {
+                score_for.entry(hist).or_insert(new as u8);
+            }
         }
 
         for (&chunk, &score) in &score_for {
